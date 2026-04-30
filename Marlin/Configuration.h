cdf86b525f05e7fd8f2646c83387ddb1d44f34f4
Marcin Q <51169435+Marcepanb@users.noreply.github.com>
2026-04-30 09:02:17 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2618,17 +2618,17 @@
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED    0//255 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "PETG"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED     90
+#define PREHEAT_2_LABEL       "TPU"
+#define PREHEAT_2_TEMP_HOTEND 230
+#define PREHEAT_2_TEMP_BED     70
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED    0//255 // Value from 0 to 255
 
 // #define EXTRA_PREHEAT_LABELS   // Enable LCD Menu to Configure 2 Extra Preheat Materials
 #if ENABLED(EXTRA_PREHEAT_LABELS)
-  #define PREHEAT_3_LABEL       "TPU"
-  #define PREHEAT_3_TEMP_HOTEND 230
-  #define PREHEAT_3_TEMP_BED     70
+  #define PREHEAT_3_LABEL       "PETG"
+  #define PREHEAT_3_TEMP_HOTEND 240
+  #define PREHEAT_3_TEMP_BED     90
   #define PREHEAT_3_TEMP_CHAMBER 35
   #define PREHEAT_3_FAN_SPEED    0//255 // Value from 0 to 255
 
